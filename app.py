97d352c3f9c82956459c7e3b60ce9e9c93ad8214
Shivam Khunger <shivamkhunger7643@gmail.com>
2025-01-16 00:40:12 +0530

    Deployment of Model

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
 # Set page configuration
 st.set_page_config(page_title="Prediction of Disease Outbreaks",
                    layout="wide",
